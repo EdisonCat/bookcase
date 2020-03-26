3fa3d1e7be7ac3ed833f6acac063624cfcb51134
EdisonCat <edisonguocat@gmail.com>
2020-03-26 04:24:24 -0700

    update mongodb for redis caching

--- src/main/java/com/edison/bookcase/dao/MongoDBConfig.java
+++ src/main/java/com/edison/bookcase/dao/MongoDBConfig.java
@@ -1,13 +1,16 @@
 package com.edison.bookcase.dao;
 
-import com.edison.bookcase.model.Book;
 import com.edison.bookcase.service.BookMongoDBService;
+import com.mongodb.MongoClient;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.util.Date;
 
 @EnableMongoRepositories(basePackageClasses = BookMongoDBService.class)
 @Configuration
@@ -16,9 +19,25 @@ public class MongoDBConfig {
     @Bean
     CommandLineRunner commandLineRunner(BookMongoDBService bookRepository) {
         return strings -> {
-            bookRepository.save(new Book(1, "LZSWDNMD", new Date(), "CSB"));
-            bookRepository.save(new Book(2, "LZSWDNMD", new Date(), "CSV"));
+//            bookRepository.save(new Book(1, "LZSWDNMD", new Date(), "CSB"));
+//            bookRepository.save(new Book(2, "LZSWDNMD", new Date(), "CSV"));
+            bookRepository.deleteAll();
         };
+    }
+
+    @Bean
+    public MongoClient mongoClient() {
+        return new MongoClient("localhost", 27017);
+    }
+
+    @Bean
+    public MongoDbFactory mongoDbFactory() {
+        return new SimpleMongoDbFactory(mongoClient(), "bookcase");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoDbFactory());
     }
 
 
