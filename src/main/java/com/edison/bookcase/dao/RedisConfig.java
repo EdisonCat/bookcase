4f3ab0620998e4bb4eaf6346801c6401c83f28d8
EdisonCat <edisonguocat@gmail.com>
2020-03-26 04:24:00 -0700

    add primary connector and template for caching

--- src/main/java/com/edison/bookcase/dao/RedisConfig.java
+++ src/main/java/com/edison/bookcase/dao/RedisConfig.java
@@ -1,45 +1,46 @@
 package com.edison.bookcase.dao;
 
-import com.edison.bookcase.service.RedisReceiver;
+import com.edison.bookcase.model.Book;
+import com.edison.bookcase.service.RedisConfigSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.util.UUID;
-import java.util.concurrent.Executors;
 
 @Configuration
 public class RedisConfig {
 
-    @Bean
-    public JedisConnectionFactory connectionFactory() {
+    @Autowired
+    private RedisConfigSerializer redisSerializer;
+
+    @Bean(name = "RedisCachingConnector")
+    @Primary
+    public JedisConnectionFactory jedisConnectionFactory() {
+        //        jedisConnectionFactory.setUsePool(true);
+//        jedisConnectionFactory.setHostName("localhost");
+//        jedisConnectionFactory.setPort(6379);
         return new JedisConnectionFactory();
     }
 
-    @Bean
-    public RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory());
-        template.setValueSerializer(new GenericToStringSerializer<>(String.class));
+    @Bean(name = "RedisCachingTemplate")
+    public RedisTemplate<String, Book> redisTemplate() {
+        RedisTemplate<String, Book> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(redisSerializer);
         return template;
     }
 
     @Bean
-    ChannelTopic topic() {
-        return new ChannelTopic(UUID.randomUUID().toString());
+    public RedisCacheManager redisCacheManager() {
+        RedisCacheManager manager = RedisCacheManager.create(jedisConnectionFactory());
+        manager.setTransactionAware(true);
+        return manager;
     }
 
-    @Bean
-    RedisMessageListenerContainer redisContainer() {
-        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory());
-        container.addMessageListener(new MessageListenerAdapter(new RedisReceiver()), topic());
-        container.setTaskExecutor(Executors.newFixedThreadPool(4));
-        return container;
-    }
 }
