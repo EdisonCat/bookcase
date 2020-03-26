f77602cf02d97200403efa383b383756f56abe96
EdisonCat <edisonguocat@gmail.com>
2020-03-26 04:23:19 -0700

    add cacheable annotation and methods

--- src/main/java/com/edison/bookcase/BookcaseApplication.java
+++ src/main/java/com/edison/bookcase/BookcaseApplication.java
@@ -2,8 +2,10 @@ package com.edison.bookcase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class BookcaseApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/edison/bookcase/api/BookController.java
+++ src/main/java/com/edison/bookcase/api/BookController.java
@@ -3,12 +3,17 @@ package com.edison.bookcase.api;
 import com.edison.bookcase.model.Book;
 import com.edison.bookcase.service.BookMongoDBService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/bookcase/mongodb")
+@CacheConfig(cacheNames = "bookCache")
 public class BookController {
 
     private final BookMongoDBService bookService;
@@ -18,29 +23,34 @@ public class BookController {
         this.bookService = bookService;
     }
 
+//    @Cacheable(key = "#bookId")
     @PostMapping
     public void addBook(@RequestBody Book book) {
         bookService.insert(book);
     }
 
+//    @Cacheable(value = "book")
     @GetMapping(path = "all")
     public List<Book> getAllBooks() {
         return bookService.findAll();
     }
 
+    @Cacheable(key = "#bookId", value = "book")
     @GetMapping(path = "{bookId}")
-    public Book getBookById(@PathVariable("bookId") Integer bookId) {
+    public Book getBookById(@PathVariable("bookId") UUID bookId) {
         if(bookService.findById(bookId).isPresent()) return bookService.findById(bookId).get();
         return null;
     }
 
+    @CacheEvict(allEntries = true)
     @DeleteMapping
     public void deleteAllBooks() {
         bookService.deleteAll();
     }
 
+    @CacheEvict(key = "#bookId")
     @DeleteMapping(path = "{bookId}")
-    public void deleteBookById(@PathVariable("bookId") Integer bookId) {
+    public void deleteBookById(@PathVariable("bookId") UUID bookId) {
         bookService.deleteById(bookId);
     }
 }
